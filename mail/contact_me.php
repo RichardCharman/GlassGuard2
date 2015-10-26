87ebe63066628cb2b71440b1028c21d6671c036b
RichardCharman <richardcharman@live.com>
2015-10-26 21:05:07 +0000

    Added mail server script

---
+++ mail/contact_me.php
@@ -0,0 +1,29 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// Create the email and send the message
+$to = 'warpedfactor@live.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$contact_subject = "Glass Guard";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$contact_message = "Thank you for contacting Glass Guard. We will respond to your inquiry shortly.";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);  // email to glass-guard
+mail($email,$contact_subject,$contact_message,$headers); // email to sender
+return true;
+?>
