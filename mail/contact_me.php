b70f9a3f9e340ffc2fcbad1d36b7534f4f677152
RichardCharman <richardcharman@live.com>
2016-02-03 11:05:34 +0000

    updated video and contact section

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,8 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'duncan@modtechserv.com'; // Your email address.
+$to = 'duncan@glass-guard.com'; // Your email address.
+$and = 'sonya@glass-guard.com';
 $email_subject = "Website Contact Form:  $name";
 $contact_subject = "Glass Guard";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
@@ -24,6 +25,7 @@ $contact_message = "Thank you for contacting Glass Guard. We will respond to you
 $headers = "From: noreply@glass-guard.com\n"; // This is the email address the generated message will be from.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);  // email to glass-guard
+mail($and,$email_subject,$email_body,$headers);  // email to glass-guard
 mail($email,$contact_subject,$contact_message,$headers); // email to sender
 return true;
 ?>
